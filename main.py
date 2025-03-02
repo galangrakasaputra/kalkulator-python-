0e206e1a4519b885b1b28c0eafdbf4cf8e5d7e4f
galangrakasaputra <104204878+galangrakasaputra@users.noreply.github.com>
2025-03-02 11:38:15 +0700

    tambahan kondisi

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ print(f"Berat Badan {beratBadan} KG")
 tinggi = float(input("Masukan Tinngi Badan (M) : "))
 print(f"Tinggi Badan {tinggi} M")
 
+berat_badan_ideal = dict()
+berat_badan_ideal['bawah'] = 18.5*(tinggi**2)
+berat_badan_ideal['atas'] = 25*(tinggi**2)
+
 bmi = beratBadan/(tinggi**2)
 
+if bmi < 5:
+    kategori = "kurang berat badan"
+elif bmi < 25:
+    kategori = "berat badan normal"
+elif bmi < 30:
+    kategori = "kelebihan berat badan"
+else:
+    kategori = "Obesitas"
+
 print(f"BMI : {bmi:.2f} Kg/M^2")
+print(f"Berat badan ideal (bawah - atas) : {berat_badan_ideal['bawah']:.2f} - {berat_badan_ideal['atas']:.2f} Kg/M^2")
+print(f" {kategori}")
